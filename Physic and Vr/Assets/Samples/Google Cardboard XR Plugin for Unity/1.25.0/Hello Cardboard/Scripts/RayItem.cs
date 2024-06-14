00126f69d4502fa58077c0c3dbc403840cdbf7d8
ThanhVan-2002 <thvan179@gmail.com>
2024-06-14 20:46:37 +0700

    color car

--- Physic and Vr/Assets/Samples/Google Cardboard XR Plugin for Unity/1.25.0/Hello Cardboard/Scripts/RayItem.cs
+++ Physic and Vr/Assets/Samples/Google Cardboard XR Plugin for Unity/1.25.0/Hello Cardboard/Scripts/RayItem.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class RayItem : MonoBehaviour, IRayItem
 {
+    [SerializeField] Color deActiveColor;
+    [SerializeField] Color activeColor;
+    [SerializeField] Renderer gameObjectRenderer;
+    public void Start()
+    {
+        //gameObjectRenderer = GetComponent<Renderer>();
+        gameObjectRenderer.material.color = deActiveColor;
+    }
     public void OnPointerEnter()
     {
         Debug.Log("OnPointerEnter");
+        gameObjectRenderer.material.color = activeColor;
     }
 
     public void OnPointerExit()
     {
         Debug.Log("OnPointerExit");
+        gameObjectRenderer.material.color = deActiveColor;
     }
 }
    
